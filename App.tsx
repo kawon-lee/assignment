bf3d2cdf1fefe0ba6f058685e87b0cd0abde9833
kawon-lee <boj171124@gmail.com>
2026-05-11 15:08:28 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -80,7 +80,7 @@ function AppContent() {
       content.push(
         <View style={styles.card} key={i}>
           <ImageBackground
-            source={require('../../mars/assignment/ios/assignment/assets/images/bkimg.png')}
+            source={require('./assets/images/bkimg.png')}
             style={{
               height: 233,
               justifyContent: 'space-between',
